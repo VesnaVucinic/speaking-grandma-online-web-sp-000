84e98085b5ea2a6e0f51d02945cdbf202b0b92da
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-10-03 13:51:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,16 +1,16 @@
 def speak_to_grandma(phrase)
-  if phrase.upcase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+  if phrase.upcase == 'I LOVE YOU GRANDMA!'
+    puts 'I LOVE YOU TOO PUMPKIN!'
   #elsif phrase.upcase() != "I LOVE YOU GRANDMA!"
     #elsif phrase != phrase.upcase()
     elsif phrase == 'Hi Nana, how are you?'
-    puts "WHAT? NO, NOT SINCE 1938!"
+    puts 'WHAT? NO, NOT SINCE 1938!'
   else
-    puts "HUH?! SPEAK UP, SONNY!"
+    puts 'HUH?! SPEAK UP, SONNY!'
   end
 end
 
-speak_to_grandma("I LOVE YOU GRANDMA!")
+speak_to_grandma('I LOVE YOU GRANDMA!')
 
 
 
